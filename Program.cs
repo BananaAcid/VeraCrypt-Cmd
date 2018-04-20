f842104b8ff05db29426f71d4e46bef8f6b02fd6
Nabil.Redmann <repo@bananaacid.de>
2018-04-21 00:24:24 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeraCrypt_Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find VeraCrypt mounts and drive letter.");
+            GetMounts().Wait();
+        }
+
+
+        private static async Task GetMounts()
+        {
+            var found = false;
+            var a = await Utils.VcGetMounts.getMounted();
+
+
+            try
+            {
+                foreach (var item in a)
+                {
+                    if (!found) // on first iteration
+                        Console.WriteLine("Letter\tMount source");
+
+                    Console.WriteLine(item.Key + "\t" + item.Value);
+
+                    found = true;
+                }
+            }
+            catch
+            { }
+
+            if (!found)
+                Console.WriteLine("No mounts found.");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Vera Crypt mount finder commandline tool
+__Source code and binary__
+
+    C:\VeraCrypt Cmd\bin\Release\VeraCrypt Cmd.exe
+    Find VeraCrypt mounts and drive letter.
+    Letter  Mount source
+    K       \??\C:\Users\BananaAcid\OneDrive\Test-Archive.vc
+
+Relates to https://github.com/BananaAcid/Selfcontained-C-Sharp-WPF-compatible-utility-classes
+
+MIT license used.
