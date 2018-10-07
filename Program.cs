61ce9bf765f14ed57db436f73b609207656206aa
Nabil.Redmann <repo@bananaacid.de>
2018-10-07 23:59:14 +0200

    added more output formats

--- Program.cs
+++ Program.cs
@@ -1,21 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace VeraCrypt_Cmd
 {
     class Program
     {
+        enum OutMode
+        {
+            TEXT,
+            JSON,
+            CSV
+        }
+        static OutMode outModeStrean = OutMode.TEXT;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Find VeraCrypt mounts and drive letter. use /x for extended info.");
-            
+            if (Environment.GetCommandLineArgs().Contains<string>("/json"))
+                outModeStrean = OutMode.JSON;
+            if (Environment.GetCommandLineArgs().Contains<string>("/csv"))
+                outModeStrean = OutMode.CSV;
+
+
+
+            switch (outModeStrean)
+            {
+                case OutMode.TEXT:
+                    {
+                        Console.WriteLine("{0} v.{1}",
+                            System.Reflection.Assembly.GetExecutingAssembly().GetName().Name,
+                            System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()
+                        );
+
+                        Console.WriteLine("Find VeraCrypt mounts and drive letters.\nVeraCrypt must be installed. Use /x for extended info, /json for json output, /csv for comma seperated values\n");
+
+                        break;
+                    }
+                case OutMode.JSON:
+                    {
+                        Console.WriteLine("{{\n\t\"version\": \"{0}\",\n\t\"mounts\": [",
+                            System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()
+                        );
+                        break;
+                    }
+                case OutMode.CSV:
+                    { break; }
+            }
+
             if (Environment.GetCommandLineArgs().Contains<string>("/x"))
                 GetMounts().Wait();
             else
                 GetMountsSimple().Wait();
+
+            if (outModeStrean == OutMode.TEXT)
+                Console.WriteLine("\n");
+            else if (outModeStrean == OutMode.JSON)
+                Console.WriteLine("\t]\n}");
         }
 
 
@@ -28,20 +70,37 @@ namespace VeraCrypt_Cmd
 
             try
             {
+                // header: CSV needs it, even if there is nothing found
+                if (outModeStrean == OutMode.CSV)
+                    Console.WriteLine("letter, volumeName");
+
                 foreach (var item in a)
                 {
+                    // header
                     if (!found) // on first iteration
-                        Console.WriteLine("letter\tMount source (volumeName)");
+                    {
+                        if (outModeStrean == OutMode.TEXT)
+                            Console.WriteLine("Letter  Mount source (volumeName)");
+                    }
 
-                    Console.WriteLine(item.Key + "\t" + item.Value.volumeName);
+                    lineOut(new Dictionary<string, object> {
+                        { "letter", item.Key},
+                        { "volumeName", item.Value.volumeName}
+                    }, found);
+                    //Console.WriteLine(item.Key + "\t" + item.Value.volumeName);
 
                     found = true;
                 }
             }
-            catch
-            { }
+            catch (Exception ex)
+            {
+                lineOut(new Dictionary<string, object> {
+                    { "error", ex.Message}
+                }, found);
+                //Console.WriteLine(ex.Message);
+            }
 
-            if (!found)
+            if (!found && outModeStrean == OutMode.TEXT)
                 Console.WriteLine("No mounts found.");
         }
 
@@ -57,42 +116,156 @@ namespace VeraCrypt_Cmd
 
             try
             {
+                // header: CSV needs it, even if there is nothing found
+                if (outModeStrean == OutMode.CSV)
+                    Console.Write("{0}\n", String.Join(", ", keys));
+
                 foreach (var item in a)
                 {
+                    // header
                     if (!found) // on first iteration
                     {
-                        foreach (var prop in keys)
-                            Console.Write("{0}\t", prop);
-
-                        Console.Write("\n");
+                        if (outModeStrean == OutMode.TEXT)
+                            Console.Write("{0}\n", String.Join("  ", keys));
                     }
 
+                    var row = new Dictionary<string, object>();
+
+                    // data
                     foreach (var pKey in keys)
                     {
                         var prop = item.Value.GetType().GetProperty(pKey);
                         try
                         {
                             var x = prop.GetValue(item.Value);
-                            Console.Write("{0}\t", x.ToString().Equals("") ? "?" : x);
+
+                            row.Add(pKey, x);
+                            // Console.Write("{0}\t", x.ToString().Equals("") ? "?" : x);
                         }
                         catch
                         {
-                            Console.Write("?\t");
+                            //Console.Write("?\t");
+                            row.Add(pKey, null);
                         }
                     }
 
-                    Console.Write("\n");
+                    lineOut(row, found);
+                    //Console.Write("\n");
+
                     found = true;
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                lineOut(new Dictionary<string, object> {
+                    { "error", ex.Message}
+                }, found);
+                //Console.WriteLine(ex.Message);
             }
 
-            if (!found)
+            if (!found && outModeStrean == OutMode.TEXT)
                 Console.WriteLine("No mounts found.");
         }
 
-     }
+
+
+
+        /// <summary>
+        /// Own way of a stream writer, we do not cache up lines to generate a JSON file (line by line)
+        /// </summary>
+        /// <param name="row">key value pairs of drive properties</param>
+        /// <param name="isPreceeded">for JSON: if there was a block outputted before (JSON: no trailing comma allowed)</param>
+        private static void lineOut(Dictionary<string, object> row, bool isPreceeded = false)
+        {
+            switch (outModeStrean)
+            {
+                case OutMode.JSON:
+                    {
+                        var rows = row.Select(x =>
+                        {
+                            if (x.Value is System.String || x.Value is System.Char)
+                                return String.Format("\t\t\t\"{0}\": \"{1}\"", x.Key, x.Value.ToString());
+                            else if (x.Value.Equals(null))
+                                return String.Format("\t\t\t\"{0}\": {1}", x.Key, "null");
+                            else if (x.Value is System.Boolean)
+                                return String.Format("\t\t\t\"{0}\": {1}", x.Key, (bool)x.Value ? "true" : "false");
+                            else
+                                return String.Format("\t\t\t\"{0}\": {1}", x.Key, (ulong)x.Value);
+                        });
+
+                        Console.Write("\t\t{0}\n\t\t{{\n{1}\n\t\t}}\n", isPreceeded ? "," : "", String.Join(",\n", rows));
+
+                        break;
+                    }
+                case OutMode.TEXT:
+                    {
+                        var rows = row.Select(x =>
+                        {
+                            // min field width is header 
+                            var val = x.Value.ToString().Equals("") ? "?" : x.Value;
+                            var diff = x.Key.Length - val.ToString().Length;
+                            val = diff > 0 ? val + new String(' ', diff) : val;
+
+                            return String.Format("{0}", val);
+                        });
+                        Console.Write("{0}\n", String.Join("  ", rows));
+
+                        break;
+                    }
+                case OutMode.CSV:
+                    {
+                        var rows = row.Select(x =>
+                        {
+                            return Csv.Escape( x.Value.ToString() );
+                        });
+                        Console.Write("{0}\n", String.Join(", ", rows));
+                        break;
+                    }
+                default:
+                    {
+                        Console.Write("unknown output type");
+                        return;
+                    }
+            }
+        }
+
+    }
+
+
+    /// <summary>
+    /// Class to escape CSV fields
+    /// </summary>
+    /// <see cref="https://stackoverflow.com/a/769713/1644202"/>
+    public static class Csv
+    {
+        public static string Escape(string s)
+        {
+            if (s.Contains(QUOTE))
+                s = s.Replace(QUOTE, ESCAPED_QUOTE);
+
+            if (s.IndexOfAny(CHARACTERS_THAT_MUST_BE_QUOTED) > -1)
+                s = QUOTE + s + QUOTE;
+
+            return s;
+        }
+
+        public static string Unescape(string s)
+        {
+            if (s.StartsWith(QUOTE) && s.EndsWith(QUOTE))
+            {
+                s = s.Substring(1, s.Length - 2);
+
+                if (s.Contains(ESCAPED_QUOTE))
+                    s = s.Replace(ESCAPED_QUOTE, QUOTE);
+            }
+
+            return s;
+        }
+
+
+        private const string QUOTE = "\"";
+        private const string ESCAPED_QUOTE = "\"\"";
+        private static char[] CHARACTERS_THAT_MUST_BE_QUOTED = { ',', '"', '\n' };
+    }
+
 }
--- Readme.md
+++ Readme.md
@@ -5,9 +5,50 @@ Download binary only: [bin/Release/VeraCrypt Cmd.exe](https://github.com/BananaA
 __Source code and binary__
 
     C:\VeraCrypt Cmd\bin\Release\VeraCrypt Cmd.exe
-    Find VeraCrypt mounts and drive letter.
-    Letter  Mount source
-    K       \??\C:\Users\BananaAcid\OneDrive\Test-Archive.vc
+    VeraCrypt Cmd v.1.1.0.0
+    Find VeraCrypt mounts and drive letters.
+    VeraCrypt must be installed. Use /x for extended info, /json for json output, /csv for comma seperated values
+     
+    letter  Mount source (volumeName)
+    K       \??\C:\Users\BananaAcid\test.vc
+    M       \??\C:\Users\BananaAcid\test 2.vc
+
+
+- `/x` get extended infos: letter, truecryptMode, diskLength, volumeLabel, volumeName
+- `/text` is the default, generate user readable output
+- `/json` sets the output generate JSON
+- `/csv` sets the output to generate properly escaped CSV
+
+_truecryptMode_: if vera crypt loaded a TrueCrypt container in compatibility mode
+
+
+Version 1.1.0.0 added the output modes (code got messier). For a simpler usecase, select version 1.0.0.0.
+
+
+## Output for `/json /x`
+
+    C:\VeraCrypt Cmd\bin\Release\VeraCrypt Cmd.exe /json /x
+    {
+            "version": "1.1.0.0",
+            "mounts": [
+                     
+                    {
+                            "letter": "K",
+                            "truecryptMode": false,
+                            "diskLength": 4980736,
+                            "volumeLabel": "",
+                            "volumeName": "\??\C:\Users\BananaAcid\test.vc"
+                    }
+                    ,
+                    {
+                            "letter": "M",
+                            "truecryptMode": false,
+                            "diskLength": 4980736,
+                            "volumeLabel": "",
+                            "volumeName": "\??\C:\Users\BananaAcid\test 2.vc"
+                    }
+            ]
+    }
 
 Relates to https://github.com/BananaAcid/Selfcontained-C-Sharp-WPF-compatible-utility-classes
 
